dda3b87afc6dfdd4394ddc5cfa1c277cd59f07a8
Clara Derot <90332021+claradrt@users.noreply.github.com>
2021-12-05 12:44:25 +0100

    Routes, Bootstrap, NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Quiz from "./routes/quiz";
+import Vocabulary from "./routes/vocabulary-list";
+import NavBar from "./NavBar.js";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Quiz />} />
+          <Route path="/vocabulary" element={<Vocabulary />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/routes/vocabulary-list.jsx
@@ -0,0 +1,3 @@
+export default function Vocabulary() {
+  return "Hello from Vocabulary route";
+}

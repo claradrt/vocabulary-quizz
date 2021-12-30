35df71bc3834e75ffcd5826ae053f9c1003b06e6
Clara Derot <90332021+claradrt@users.noreply.github.com>
2021-12-30 11:22:04 +0100

    Responsiveness of quiz

--- src/TermTranslation.js
+++ src/TermTranslation.js
@@ -5,35 +5,39 @@ import AnswerForm from "./AnswerForm";
 export default function TermTranslation(props) {
   if (props.meaning === "") {
     return (
-      <div className="TermTranslation d-inline-flex">
-        <div>
-          <h2>Word</h2>
-          <div className="wordToTranslate">{props.word}</div>
-        </div>
-        <div>
-          <h2>Meaning</h2>
-          <AnswerForm
-            answer={props.answer}
-            newWordToTranslate={props.newWordToTranslate}
-            addPointToScore={props.addPointToScore}
-          />
+      <div className="TermTranslation">
+        <div className="row justify-content-center gx-1">
+          <div className="col-sm">
+            <h2>Word</h2>
+            <div className="wordToTranslate">{props.word}</div>
+          </div>
+          <div className="col-sm">
+            <h2>Meaning</h2>
+            <AnswerForm
+              answer={props.answer}
+              newWordToTranslate={props.newWordToTranslate}
+              addPointToScore={props.addPointToScore}
+            />
+          </div>
         </div>
       </div>
     );
   } else {
     return (
-      <div className="TermTranslation d-inline-flex">
-        <div>
-          <h2>Word</h2>
-          <AnswerForm
-            answer={props.answer}
-            newWordToTranslate={props.newWordToTranslate}
-            addPointToScore={props.addPointToScore}
-          />
-        </div>
-        <div>
-          <h2>Meaning</h2>
-          <div className="wordToTranslate">{props.meaning}</div>
+      <div className="TermTranslation">
+        <div className=" row justify-content-center gx-1">
+          <div className="col-sm">
+            <h2>Word</h2>
+            <AnswerForm
+              answer={props.answer}
+              newWordToTranslate={props.newWordToTranslate}
+              addPointToScore={props.addPointToScore}
+            />
+          </div>
+          <div className="col-sm">
+            <h2>Meaning</h2>
+            <div className="wordToTranslate">{props.meaning}</div>
+          </div>
         </div>
       </div>
     );

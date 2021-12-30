28822d5fc6464b815c2fa2d991141df0be003261
Clara Derot <90332021+claradrt@users.noreply.github.com>
2021-12-30 10:20:15 +0100

    CSS styling and responsiveness

--- src/AddWord.js
+++ src/AddWord.js
@@ -33,29 +33,29 @@ export default function AddWord(props) {
   return (
     <div className="mt-4 AddWord mx-auto">
       <form
-        className="row my-3 justify-content-center"
+        className="row my-3 gy-1 gx-1 gx-md-3 justify-content-center text-center"
         onSubmit={handleFormSubmit}
       >
-        <div className="col-6 col-md-5 text-end">
+        <div className="col-sm col-md-5 word-input">
           <input
             type="text"
             placeholder="Word..."
-            className="text-center text-capitalize"
+            className="text-center text-capitalize "
             onChange={handleWordInputChange}
             value={newWord}
             autoFocus
           />
         </div>
-        <div className="col-6 col-md-5 text-start">
+        <div className="col-sm col-md-5">
           <input
             type="text"
             placeholder="Meaning..."
-            className="text-center text-capitalize"
+            className="text-center text-capitalize meaning-input"
             onChange={handleMeaningInputChange}
             value={newMeaning}
           />
-          <div className="text-center d-inline-block">
-            <button type="submit" className="align-self-center add-icon">
+          <div className="text-center add-btn-wrapper">
+            <button type="submit" className="align-self-center">
               <FontAwesomeIcon icon={faPlus} color="#757575" />
             </button>
           </div>

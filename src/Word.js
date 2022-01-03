51e4b234598410c39664152284dd4071bcab4624
Clara Derot <90332021+claradrt@users.noreply.github.com>
2022-01-03 14:25:22 +0100

    Add parent checkbox

--- src/Word.js
+++ src/Word.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Word.css";
 import Checkbox from "@mui/material/Checkbox";
 
 export default function Word(props) {
+  const [checked, setChecked] = useState(props.parentCheckboxState);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+    if (event.target.checked) {
+      props.wordIsSelected(props.wordIndex);
+    } else {
+      props.wordIsUnselected(props.wordIndex);
+    }
+  };
 
   return (
     <div className="border word-wrapper">
@@ -11,6 +21,10 @@ export default function Word(props) {
           <div className="word text-capitalize">{props.word}</div>
           <div className="meaning text-capitalize">{props.meaning}</div>
         </div>
+        <div className="checkbox-wrapper col-6 align-self-center pe-3">
+          <label className="checkbox">
+            <Checkbox checked={checked} onChange={handleChange} />
+          </label>
         </div>
       </div>
     </div>
--- src/routes/vocabulary-list.jsx
+++ src/routes/vocabulary-list.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import Word from "../Word";
 import AddWord from "../AddWord";
 import "../VocabularyList.css";
+
+import Checkbox from "@mui/material/Checkbox";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
@@ -13,8 +16,18 @@ export default function Vocabulary() {
   });
   const [selectedWordsIndex, setSelectedWordsIndex] = useState([]);
   const [showDeleteOption, setShowDeleteOption] = useState(false);
-
+  const [parentCheckboxState, setParentCheckboxState] = useState({
+    checked: false,
+    indeterminate: false,
+  });
   const [addWord, setAddWord] = useState(false);
+
+  function updateParentCheckboxStatus(checked, indeterminate) {
+    const checkboxStatus = { ...parentCheckboxState };
+    checkboxStatus.checked = checked;
+    checkboxStatus.indeterminate = indeterminate;
+    setParentCheckboxState(checkboxStatus);
+  }
 
   function addNewWord(word) {
     vocabularyList.push(word);
@@ -45,20 +58,32 @@ export default function Vocabulary() {
   }
 
   function wordIsSelected(wordIndex) {
-    selectedWordsIndex.push(wordIndex);
-    setSelectedWordsIndex(selectedWordsIndex);
-    console.log("Array of selected words indexes:", selectedWordsIndex);
+    let wordsIndex = [...selectedWordsIndex];
+    wordsIndex.push(wordIndex);
+    setSelectedWordsIndex(wordsIndex);
     setShowDeleteOption(true);
+    if (wordsIndex.length === vocabularyList.length) {
+      updateParentCheckboxStatus(true, false);
+    } else {
+      updateParentCheckboxStatus(false, true);
+    }
   }
 
   function wordIsUnselected(wordIndex) {
-    let index = selectedWordsIndex.indexOf(wordIndex);
-    selectedWordsIndex.splice(index, 1);
-    setSelectedWordsIndex(selectedWordsIndex);
-    console.log("Array of selected words indexes:", selectedWordsIndex);
-    if (selectedWordsIndex.length === 0) {
+    let wordsIndex = [...selectedWordsIndex];
+    let index = wordsIndex.indexOf(wordIndex);
+    wordsIndex.splice(index, 1);
+    setSelectedWordsIndex(wordsIndex);
+    if (wordsIndex.length === 0) {
       setShowDeleteOption(false);
+      updateParentCheckboxStatus(false, false);
+    } else {
+      updateParentCheckboxStatus(false, true);
     }
+  }
+
+  function handleChange(event) {
+    updateParentCheckboxStatus(event.target.checked, false);
   }
 
   return (
@@ -76,6 +101,20 @@ export default function Vocabulary() {
           +Add new word
         </div>
       )}
+      <div className="icon-wrapper">
+        {showDeleteOption && (
+          <span className="delete-wrapper">
+            <span className="delete-icon" onClick={deleteSelection}>
+              <FontAwesomeIcon icon={faTrash} color="#4A4A4B" />
+            </span>
+          </span>
+        )}
+        <Checkbox
+          checked={parentCheckboxState.checked}
+          indeterminate={parentCheckboxState.indeterminate}
+          onChange={handleChange}
+        />
+      </div>
       {vocabularyList &&
         vocabularyList.map((wordObject, index) => {
           return (
@@ -86,6 +125,7 @@ export default function Vocabulary() {
                 wordIndex={index}
                 wordIsSelected={wordIsSelected}
                 wordIsUnselected={wordIsUnselected}
+                parentCheckboxState={parentCheckboxState.checked}
               />
             </div>
           );

ae276cba1e1709a138298b3e3d094dddc4a08fa7
Clara Derot <90332021+claradrt@users.noreply.github.com>
2022-01-07 15:02:55 +0100

    Customise confirmation modal

--- src/ConfirmationModal.js
+++ src/ConfirmationModal.js
@@ -7,10 +7,12 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function ConfirmationModal(props) {
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
     <div>
@@ -27,11 +29,12 @@ export default function ConfirmationModal(props) {
           <DialogContentText>This action cannot be undone.</DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button autoFocus onClick={props.handleClose}>
+          <Button autoFocus onClick={props.handleClose} variant="outlined">
             Cancel
           </Button>
-          <Button onClick={props.handleDelete} autoFocus>
-            Yes, I am sure
+          <Button onClick={props.handleDelete} autoFocus variant="contained">
+            <FontAwesomeIcon icon={faTrash} />{" "}
+            <p className="ps-2 my-0">Delete</p>
           </Button>
         </DialogActions>
       </Dialog>

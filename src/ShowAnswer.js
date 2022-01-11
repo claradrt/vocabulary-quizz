d35cd43c84c3592296f4550c53f91c5f15617f65
Clara Derot <90332021+claradrt@users.noreply.github.com>
2022-01-11 09:47:33 +0100

    Add arrow icon to next word button

--- src/ShowAnswer.js
+++ src/ShowAnswer.js
@@ -1,5 +1,9 @@
 import React from "react";
+
 import "./ShowAnswer.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function ShowAnswer(props) {
   function handleClick() {
@@ -9,7 +13,10 @@ export default function ShowAnswer(props) {
     <div className="ShowAnswer">
       <span className="answer">{props.answer}</span>
       <div className="next-word-btn" onClick={handleClick}>
-        Next word
+        <span className="next">Next word</span>
+        <span className="icon">
+          <FontAwesomeIcon icon={faArrowRight} />
+        </span>
       </div>
     </div>
   );

241566f88f17e7fd30d8fb4a942e8a070d41844c
Clara Derot <90332021+claradrt@users.noreply.github.com>
2022-01-13 15:42:49 +0100

    Create Footer component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import VocabularyTest from "./routes/vocabulary-test";
 import Vocabulary from "./routes/vocabulary-list";
 import NavBar from "./NavBar.js";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Footer from "./Footer";
 
 import "./Variables.css";
 
@@ -22,25 +20,7 @@ export default function App() {
             <Route path="/vocabulary" element={<Vocabulary />} />
           </Routes>
         </div>
-        <footer className="text-center mt-4">
-          {" "}
-          <i>
-            <FontAwesomeIcon icon={faGithub} />
-          </i>
-          <a
-            href="https://github.com/claradrt/vocabulary-quizz"
-            target="_blank"
-          >
-            Open source code
-          </a>{" "}
-          by{" "}
-          <a
-            href="https://vigilant-hypatia-9bacdb.netlify.app/index.html"
-            target="_blank"
-          >
-            Clara Dérot
-          </a>
-        </footer>
+        <Footer />
       </HashRouter>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./Footer.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+export default function Footer() {
+  return (
+    <footer className="text-center mt-4">
+      {" "}
+      <i>
+        <FontAwesomeIcon icon={faGithub} />
+      </i>
+      <a href="https://github.com/claradrt/vocabulary-quizz" target="_blank">
+        Open source code
+      </a>{" "}
+      by{" "}
+      <a
+        href="https://vigilant-hypatia-9bacdb.netlify.app/index.html"
+        target="_blank"
+      >
+        Clara Dérot
+      </a>
+    </footer>
+  );
+}
